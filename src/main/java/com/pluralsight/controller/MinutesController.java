a42feeed357095ba3a13cea5d6c03c84d37ea13b
kasahuka <kasahuka@lin66005547.corp.capgemini.com>
2019-07-01 16:30:54 +0530

    5after (added static resource configurations like pdf)

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -18,4 +18,11 @@ public class MinutesController {
 		return "addMinutes";
 	}
 	
+//	@RequestMapping(value = "/addMoreMinutes")
+//	public String addMoreMinutes(@ModelAttribute ("exercise") Exercise exercise) {
+//		
+//		System.out.println("exercising: " + exercise.getMinutes());
+//		
+//		return "addMinutes";
+//	}
 }
